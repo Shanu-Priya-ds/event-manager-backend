85288e8777aa3fe1dcc7debd955aa884f80f1fee
Shanu Priya <priyads12@gmail.com>
2026-06-05 02:05:02 -0400

    Added forontend url in .env file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const routes= require("./routes");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:process.env.FRONTEND_URL
 }));
 //routes
 app.use("/api",routes);
